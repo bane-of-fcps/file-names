4e75f1d4d7b3a1d27772d3078c2bda133887f17d
bane-of-fcps <69486675+bane-of-fcps@users.noreply.github.com>
2020-08-16 17:17:50 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 cls
 start FirefoxPortable.exe -height 256
-ssh -o "StrictHostKeyChecking no" -D 9999 fcps@aws1.bruhmoment.org -fN -i bruh.txt
+ssh -o "StrictHostKeyChecking no" -D 9999 fcps@aws2.bruhmoment.org -fN -i bruh.txt
