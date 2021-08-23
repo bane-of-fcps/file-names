7bece8f0b1b74dbc2dd4c78aa86dc45f1007423a
bane-of-fcps <69486675+bane-of-fcps@users.noreply.github.com>
2021-08-23 17:38:09 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 cls
-curl aws2.bruhmoment.org:5000
-start FirefoxPortable.exe -height 256
-ssh -o "StrictHostKeyChecking no" -D 9999 fcps@aws2.bruhmoment.org -fN -i bruh.txt
+start FF/FirefoxPortable.exe -height 256
+cls
+echo Click "Log In" on Firefox
