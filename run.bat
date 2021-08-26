58fa9199ab63db267ba06725fb81984ec706d496
bane-of-fcps <69486675+bane-of-fcps@users.noreply.github.com>
2021-08-26 19:56:06 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 cls
 start FF/FirefoxPortable.exe -height 480 -width 640
 cls
-ssh -L 8080:localhost:8080 bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=no
+#ssh -L 8080:localhost:8080 bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=no
+ssh -D 8080 bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=no
 echo ERROR
