5755c350d01d0be4fee33a1da148a2d83697cda5
bane-of-fcps <69486675+bane-of-fcps@users.noreply.github.com>
2021-08-28 12:22:11 -0400

    Update mc.bat

--- mc.bat
+++ mc.bat
@@ -1,7 +1,7 @@
 cls
-START "DO NOT CLOSE" ssh bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=no -D 8000
-
-mc/pypy3.exe -m ensurepip
-mc/pypy3.exe -m pip install pysocks
-mc/pypy3.exe portablemc.py --main-dir ./main --work-dir ./work start -u %USERNAME% 1.17.1
+start cmd /k "ssh bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=no -D 8000"
+cd mc
+pypy3.exe -m ensurepip
+pypy3.exe -m pip install pysocks
+pypy3.exe portablemc.py --main-dir ./main --work-dir ./work start -u %USERNAME% 1.17.1
 echo ERROR
