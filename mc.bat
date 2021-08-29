a76a0705a92c9bd225b59eacbe0fea000b5ff236
bane-of-fcps <69486675+bane-of-fcps@users.noreply.github.com>
2021-08-29 18:13:07 -0400

    Update mc.bat

--- mc.bat
+++ mc.bat
@@ -2,5 +2,5 @@ start cmd /k "ssh bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=
 cd mc
 pypy3.exe -m ensurepip
 pypy3.exe -m pip install pysocks
-pypy3.exe portablemc.py --main-dir ./main --work-dir ./work start -u %USERNAME% fabric:1.17.1
+pypy3.exe portablemc.py --main-dir ./main --work-dir ./work start -u %USERNAME% fabric:1.17.1 --jvm-args Xmx500M
 echo ERROR
