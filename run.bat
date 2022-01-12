91c8233905716fec0193b4f401401f6922dd0767
bane-of-fcps <69486675+bane-of-fcps@users.noreply.github.com>
2022-01-11 19:45:42 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 cls
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/bane-of-fcps/file-names/master/bruh2.txt -OutFile bruh2.txt"
 start FF/FirefoxPortable.exe -height 480 -width 640
 cls
-ssh bruh@aws3.bruhmoment.org -i bruh.txt -o StrictHostKeyChecking=no -D 8000
+ssh bruh@aws3.bruhmoment.org -i bruh2.txt -o StrictHostKeyChecking=no -D 8000
 echo ERROR
